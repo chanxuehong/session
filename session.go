b93551fb2a4e6cbb67cccb491a4c3c2bc46595a5
chanxuehong <chanxuehong@gmail.com>
2014-05-30 22:48:48 +0800

    no message

--- session.go
+++ session.go
@@ -1,9 +1,17 @@
+// version: 1.2.3
+
+// session implements a simple memory-based session container.
+//
+//  NOTE: Suggestion is the number of cached elements does not exceed 10w,
+//  because a large number of elements for runtime.GC () is a burden.
+//  More than 10w can consider memcache, redis ...
+//
 package session
 
 import (
 	"errors"
 	"fmt"
-	"github.com/chanxuehong/session/list" //这个 list 可以复用 Element 本身, 而不仅仅是 Element.Value
+	"github.com/chanxuehong/session/list" // this list can be reused Element itself, rather than just Element.Value
 	"sync"
 	"time"
 )
@@ -14,10 +22,12 @@ var (
 )
 
 const (
-	// 设置下面的最大限制主要是为了防止 int64 溢出.
-	// Expiration = time.Now().Unix() + maxMaxAge
-	// 这样除非是恶意更改系统时间, 否则基本都能正常工作
-	maxMaxAge int = 60 * 60 * 24 * 365.2425 * 60 // seconds, 60 years
+	// Set below the maximum limit is to prevent int64 overflow, for
+	// Expiration = time.Now().Unix() + maxAge
+	// This can work under normal circumstances,
+	// unless deliberately modified the system time to
+	// 292277026536-12-05 02:18:07 +0000 UTC
+	maxAgeLimit int64 = 60 * 60 * 24 * 365.2425 * 60 // seconds, 60 years
 )
 
 //
@@ -43,59 +53,74 @@ const (
 //      | key  |value-+---------------------------------------+
 //      +------+------+
 //
-// 对于 cache 来说: key == cache[key].key
+// Principle:
+//   1. len(cache) == lruList.Len();
+//   2. for Element of lruList, we get cache[Element.Key] == Element;
+//   3. in the list lruList and freeList, the younger element
+//      is always in front of the older elements;
 //
 
+// Storage is safe for concurrent use by multiple goroutines.
 type Storage struct {
 	mu sync.Mutex
 
-	freeList *list.List               // 利旧的 list, 在这个链表上的元素都是 cache 里没有的
-	lruList  *list.List               // 在用的 list, lruList.Len() == len(cache)
-	cache    map[string]*list.Element // key == cache[key].key
+	freeList *list.List
+	lruList  *list.List
+	cache    map[string]*list.Element
 
-	maxAge int64 // seconds; Storage 里的 item 的生命期.
-
-	// 对于过期的 list.Element:
-	// 如果 lruList.Len()+freeList.Len() <= capacity, 则不物理删除, 只是移动到 freeList 链表上;
-	// 如果 lruList.Len()+freeList.Len() > capacity, 物理删除超过部分的.
-	capacity int
+	maxAge              int64 // element of lruList effective time
+	capacity            int
+	gcIntervalResetChan chan time.Duration
 }
 
-// 创建并初始化一个 Storage
-//  maxAge:     seconds; Storage 里的 item 的生命期, 最大为60年, 不能调整.
-//  gcInterval: 垃圾收集的时间间隔, 不能小于 1 分钟, 不能调整.
-//  capacity:   容量, 在这个 capacity 内的 item 不会被 gc() 物理清除, 内存复用考虑.
-func New(maxAge int, gcInterval time.Duration, capacity int) *Storage {
-	if maxAge <= 0 {
-		panic(fmt.Sprintf("session.New: maxAge must be > 0 and now == %d", maxAge))
-	} else if maxAge > maxMaxAge {
-		panic(fmt.Sprintf("session.New: maxAge must be <= %d and now == %d", maxMaxAge, maxAge))
+// New returns an initialized Storage.
+//  maxAge:     seconds; the max age of item in Storage, the maximum is 60 years, and can not be adjusted.
+//  capacity:   item within the capacity range will not be physically removed by GC , memory reuse considered.
+//  gcInterval: GC interval, the minimal is 1 minute.
+func New(maxAge, capacity int, gcInterval time.Duration) *Storage {
+	_maxAge := int64(maxAge)
+	if _maxAge <= 0 {
+		panic(fmt.Sprintf("session.New: maxAge must be > 0 and now == %d", _maxAge))
+	} else if _maxAge > maxAgeLimit {
+		panic(fmt.Sprintf("session.New: maxAge must be <= %d and now == %d", maxAgeLimit, _maxAge))
 	}
-	if gcInterval < time.Minute {
-		panic(fmt.Sprintf("session.New: gcInterval must be >= %d(time.Minute) and now == %d", time.Minute, gcInterval))
-	}
+
 	if capacity <= 0 {
 		panic(fmt.Sprintf("session.New: capacity must be > 0 and now == %d", capacity))
+	}
+
+	if gcInterval < time.Minute {
+		panic(fmt.Sprintf("session.New: gcInterval must be >= %d(time.Minute) and now == %d", time.Minute, gcInterval))
 	}
 
 	s := &Storage{
 		lruList:  list.New(),
 		freeList: list.New(),
 		cache:    make(map[string]*list.Element, capacity),
-		maxAge:   int64(maxAge),
+		maxAge:   _maxAge,
 		capacity: capacity,
 	}
 	for i := 0; i < capacity; i++ {
 		s.freeList.PushFront(&list.Element{})
 	}
 
-	// 启用一个 goroutine 来做 gc
+	// new goroutine for gc service
 	go func() {
-		gcTick := time.Tick(gcInterval)
+		gc_interval := gcInterval
+		var gcTick *time.Ticker
+
+	NEW_GC_INTERVAL:
 		for {
-			select {
-			case <-gcTick:
-				s.gc()
+			gcTick = time.NewTicker(gc_interval)
+			for {
+				select {
+				case gc_interval = <-s.gcIntervalResetChan:
+					gcTick.Stop()
+					s.gc() // call gc() immediately
+					break NEW_GC_INTERVAL
+				case <-gcTick.C:
+					s.gc()
+				}
 			}
 		}
 	}()
@@ -103,45 +128,47 @@ func New(maxAge int, gcInterval time.Duration, capacity int) *Storage {
 	return s
 }
 
-// 获取缓存的 item 个数
-func (s *Storage) Len() int {
+// Get the item cached in the Storage number.
+func (s *Storage) Len() (n int) {
 	s.mu.Lock()
-	defer s.mu.Unlock()
-	return s.lruList.Len()
+	n = s.lruList.Len()
+	s.mu.Unlock()
+	return
 }
 
-// 获取缓存中 item 的物理个数
-func (s *Storage) PhysicsLen() int {
+// Get the capacity of Storage.
+func (s *Storage) Capacity() (n int) {
 	s.mu.Lock()
-	defer s.mu.Unlock()
-	return s.lruList.Len() + s.freeList.Len() // 不会溢出, 详见 Storage.add
+	n = s.capacity
+	s.mu.Unlock()
+	return
 }
 
-// 获取缓存的当前 capacity 参数
-func (s *Storage) Capacity() int {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	return s.capacity
-}
-
-// 设置新的 capacity, 如果 capacity <= 0 则不做操作
+// Set the capacity of Storage.
+//  NOTE: if capacity <= 0, we do nothing.
 func (s *Storage) SetCapacity(capacity int) {
 	if capacity > 0 {
 		s.mu.Lock()
-		defer s.mu.Unlock()
 		s.capacity = capacity
+		s.mu.Unlock()
 	}
 }
 
-// 增加 key-value 到缓存中.
-//
-// 调用者保证 cache 中没有 "同样" 的 key, 所以可以直接增加.
-//
-// 优先复用 lruList 上过期的, 如果 lruList 上没有过期则复用 freeList 上的,
-// 如果 freeList 为空则新增.
+// Set the gc interval of Storage.
+//  NOTE: if gcInterval < time.Minute, we do nothing;
+//  and after calling this method we will soon do a GC, please avoid business peak.
+func (s *Storage) SetGCInterval(gcInterval time.Duration) {
+	if gcInterval >= time.Minute {
+		s.gcIntervalResetChan <- gcInterval
+	}
+}
+
+// add key-value to Storage.
+//  ensure that there is no the same key in Storage
 func (s *Storage) add(key string, value interface{}, timenow int64) error {
 	var e *list.Element
 
+	// Check the last element of lruList expired; if so, reused it
 	if e = s.lruList.Back(); e != nil && timenow > e.Expiration {
 		delete(s.cache, e.Key)
 
@@ -150,10 +177,10 @@ func (s *Storage) add(key string, value interface{}, timenow int64) error {
 		e.Expiration = timenow + s.maxAge
 
 		s.cache[key] = s.lruList.MoveToFront(e)
-
 		return nil
 	}
 
+	// reused the first element of freeList if it not empty
 	if s.freeList.Len() > 0 {
 		e = s.freeList.RemoveFront()
 
@@ -162,17 +189,17 @@ func (s *Storage) add(key string, value interface{}, timenow int64) error {
 		e.Expiration = timenow + s.maxAge
 
 		s.cache[key] = s.lruList.PushFront(e)
-
 		return nil
 	}
 
-	// now s.freeList.Len() == 0
-	// 如果当前 s.lruList.Len() 达到了 int 类型的最大值, 则不增加, 返回 ErrNotStored
-	// 主要是为了 s.lruList.Len() 和 s.lruList.Len()+s.freeList.Len() 不溢出.
+	// Check whether s.lruList.Len()+s.freeList.Len() has reached the maximum of int.
+	// since s.freeList.Len() == 0, so we get
+	// s.lruList.Len() == s.lruList.Len()+s.freeList.Len()
 	if s.lruList.Len()<<1 == -2 {
 		return ErrNotStored
 	}
 
+	// now create new
 	e = &list.Element{
 		Key:        key,
 		Value:      value,
@@ -183,21 +210,17 @@ func (s *Storage) add(key string, value interface{}, timenow int64) error {
 	return nil
 }
 
-// 从 s.lruList 上移除 e.
-//
-// 调用者保证 e != nil 并且 e 是 lruList 上的元素.
-//
-// 如果 s.lruList.Len()+s.freeList.Len() > s.capacity 并且
-// s.lruList.Len() < s.capacity*0.8 则物理移除, 否则移至 s.freeList
+// remove Element e from Storage.lruList
+//  ensure that e != nil and e is an element of list lruList.
 func (s *Storage) remove(e *list.Element) {
 	delete(s.cache, e.Key)
-	// e 关联的资源提前交给 runtime GC
 	e.Key = ""
 	e.Value = nil
 
-	totalLen := s.lruList.Len() + s.freeList.Len() // 不会溢出, 详见 Storage.add
-	// s.lruList.Len() < s.capacity*0.8, 用下面的实现不会溢出, 精度相对较好, 最多丢失2个数字精度;
-	// 如果转换成 float64 则有可能丢失 800 个数字的精度, 因为 float64 只有 53 位整数.
+	totalLen := s.lruList.Len() + s.freeList.Len() // does not overflow, see Storage.add
+	// A purpose of this case
+	// s.lruList.Len() < (s.capacity-s.capacity/5)
+	// is to prevent jitter
 	if totalLen > s.capacity && s.lruList.Len() < (s.capacity-s.capacity/5) {
 		s.lruList.Remove(e)
 	} else {
@@ -205,8 +228,9 @@ func (s *Storage) remove(e *list.Element) {
 	}
 }
 
-// 增加 key-value 到缓存中.
-// 如果已经存在相同的 key 元素则返回 ErrNotStored, 否则返回 nil.
+// Add key-value to Storage.
+//  if there already exists a item with the same key, then return ErrNotStored,
+//  otherwise it returns nil.
 func (s *Storage) Add(key string, value interface{}) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -214,7 +238,7 @@ func (s *Storage) Add(key string, value interface{}) error {
 	timenow := time.Now().Unix()
 
 	if e, hit := s.cache[key]; hit {
-		if timenow > e.Expiration { // 过期利旧
+		if timenow > e.Expiration {
 			//e.Key = key
 			e.Value = value
 			e.Expiration = timenow + s.maxAge
@@ -229,7 +253,7 @@ func (s *Storage) Add(key string, value interface{}) error {
 	}
 }
 
-// 设置 key-value 到缓存, 无条件的.
+// Set key-value, unconditional
 func (s *Storage) Set(key string, value interface{}) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -248,14 +272,14 @@ func (s *Storage) Set(key string, value interface{}) error {
 	}
 }
 
-// 读取 key 对应的 value, 如果不存在这样的 key 则返回 ErrNotFound
+// Get the element with key. if there is no such element with the key it returns ErrNotFound
 func (s *Storage) Get(key string) (interface{}, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	if e, hit := s.cache[key]; hit {
-		if timenow := time.Now().Unix(); timenow > e.Expiration { // 过期了
-			s.remove(e) // e != nil
+		if timenow := time.Now().Unix(); timenow > e.Expiration {
+			s.remove(e)
 			return nil, ErrNotFound
 		} else {
 			e.Expiration = timenow + s.maxAge
@@ -267,32 +291,27 @@ func (s *Storage) Get(key string) (interface{}, error) {
 	}
 }
 
-// 删除 key-value, 如果不存在这样的 key 则返回 ErrNotFound
-func (s *Storage) Delete(key string) error {
+// Delete the element with key. if there is no such element, Delete is a no-op.
+func (s *Storage) Delete(key string) (err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	if e, hit := s.cache[key]; hit {
-		s.remove(e) // e != nil
-		return nil
-	} else {
-		return ErrNotFound
+		s.remove(e)
+		return
 	}
+	return
 }
 
-// gc 清理过期的 item.
 func (s *Storage) gc() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	timenow := time.Now().Unix()
 
-	// 扫描 s.lruList, 把过期的 Element 移至 s.freeList
-	var e *list.Element
-	for e = s.lruList.Back(); e != nil; e = s.lruList.Back() {
+	for e := s.lruList.Back(); e != nil; e = s.lruList.Back() {
 		if timenow > e.Expiration {
 			delete(s.cache, e.Key)
-			// e 关联的资源提前交给 runtime GC
 			e.Key = ""
 			e.Value = nil
 			s.freeList.PushFront(s.lruList.Remove(e))
@@ -301,11 +320,10 @@ func (s *Storage) gc() {
 		}
 	}
 
-	// 如果总的容量超过了 capacity, 并且 lruList.Len() 在 capacity 的 80% 以内,
-	// 则把总容量截短到 capacity. 设计 80% 以内才截短是为了缓冲考虑.
-	totalLen := s.lruList.Len() + s.freeList.Len() // 不会溢出, 详见 Storage.add
-	// s.lruList.Len() < s.capacity*0.8, 用下面的实现不会溢出, 精度相对较好, 最多丢失2个数字精度;
-	// 如果转换成 float64 则有可能丢失 800 个数字的精度, 因为 float64 只有 53 位整数.
+	totalLen := s.lruList.Len() + s.freeList.Len() // does not overflow, see Storage.add
+	// A purpose of this case
+	// s.lruList.Len() < (s.capacity-s.capacity/5)
+	// is to prevent jitter
 	if totalLen > s.capacity && s.lruList.Len() < (s.capacity-s.capacity/5) {
 		// s.freeList.Len() > shouldRemoveNum, why?
 		for shouldRemoveNum := totalLen - s.capacity; shouldRemoveNum > 0; shouldRemoveNum-- {
